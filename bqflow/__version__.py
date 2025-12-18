ee6e6717f3659bc0f437d75a3db526d81443dfd7
Stephen Chen <40239582+schen18@users.noreply.github.com>
2025-12-18 07:59:52 -0500

    Update version and author information in __version__.py

--- bqflow/__version__.py
+++ bqflow/__version__.py
@@ -1,8 +1,8 @@
 """Version information for bqflow package."""
 
-__version__ = '0.1.0'
-__author__ = 'Your Name'
+__version__ = '0.7.0'
+__author__ = 'Stephen Chen'
 __email__ = 'your.email@example.com'
 __description__ = 'BigQuery SQL abstraction and GCS Parquet export utilities'
-__url__ = 'https://github.com/yourusername/bqflow'
+__url__ = 'https://github.com/schen18/bqflow'
 __license__ = 'MIT'
